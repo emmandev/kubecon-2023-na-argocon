ed391db2c9651b0eb731bdc8f5fa9c8be45e4823
emmandev <emmandev@users.noreply.github.com>
2024-07-07 10:52:27 +0800

    fix(eks): unauthorized error when accessing the EKS cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -10,6 +10,7 @@ module "eks" {
   cluster_version                = var.kubernetes_version
   cluster_endpoint_public_access = true
 
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
